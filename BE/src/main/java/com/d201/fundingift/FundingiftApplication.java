3ddd33758906640ecabbd032bb7f8522a3a8806e
kdozlo <eodud5730@naver.com>
2024-03-11 15:47:26 +0900

    [BE][Init] #S10P22D201-101 #done 스프링부트 프로젝트 생성

---
+++ BE/src/main/java/com/d201/fundingift/FundingiftApplication.java
@@ -0,0 +1,13 @@
+package com.d201.fundingift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundingiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundingiftApplication.class, args);
+	}
+
+}
