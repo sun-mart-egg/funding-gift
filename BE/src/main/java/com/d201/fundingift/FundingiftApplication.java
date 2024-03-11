4e71c2bb73d9c7ba1ab4fb9b85dab3f0e6274f8d
limsubinn <mymyx02@naver.com>
2024-03-11 22:22:06 +0900

    [BE][Feat] #S10P22D201-116 BaseTime 엔티티 생성

--- BE/src/main/java/com/d201/fundingift/FundingiftApplication.java
+++ BE/src/main/java/com/d201/fundingift/FundingiftApplication.java
@@ -2,8 +2,10 @@ package com.d201.fundingift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FundingiftApplication {
 
 	public static void main(String[] args) {
---
+++ BE/src/main/java/com/d201/fundingift/_common/entity/BaseTime.java
@@ -0,0 +1,26 @@
+package com.d201.fundingift._common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTime {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+}
