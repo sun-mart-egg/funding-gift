7c17786861edbc0e6294b23b514b412e7df37421
parkjonghyuk <occultism22@naver.com>
2024-03-12 17:57:55 +0900

    [FE][Design] 사용자 설정 글꼴 추가

--- FE/tailwind.config.js
+++ FE/tailwind.config.js
@@ -13,6 +13,14 @@ export default {
         'cusColor4': '#FFCD4B', // 우리가 설정한 노란색
         'cusColor5': '#40AEFF', // 우리가 설정한 파란색
         'cusColor6': '#C6F895', // 프로그레스 바 색상
+      },
+      fontFamily: {
+        cusFont1: ['cusFont1'], // 산토끼체
+        cusFont2: ['cusFont2'], // 나눔 스퀘어 BOLD
+        cusFont3: ['cusFont3'], // 나눔 스퀘어 REGULAR
+        cusFont4: ['cusFont4'], // 온글잎 시우체
+        cusFont5: ['cusFont5'], // 호빵체
+        cusFont6: ['cusFont6'], // 영도체
       }
     },
   },
