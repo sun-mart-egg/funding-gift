6ba2b490438ce2b1779709dda29c8c61972a9c99
parkjonghyuk <occultism22@naver.com>
2024-03-12 15:07:35 +0900

    [FE][Design] tailwindcss 커스텀 색상 설정

--- FE/tailwind.config.js
+++ FE/tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cusColor1': '#0802A3', // 로고, 뒤로가기 등등
+        'cusColor2': '#91D54E', // 달력 이모티콘
+        'cusColor3': '#FF7676', // 진짜 제일 많이쓰는 색깔
+        'cusColor4': '#FFCD4B', // 우리가 설정한 노란색
+        'cusColor5': '#40AEFF', // 우리가 설정한 파란색
+        'cusColor6': '#C6F895', // 프로그레스 바 색상
+      }
+    },
   },
   plugins: [],
 }
