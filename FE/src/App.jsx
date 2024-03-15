ff737bf3844c4bcd663fbc2522c69dc7821069de
신시은 <nssu422@naver.com>
2024-03-15 13:41:34 +0900

    [FE][Feat] #S10P22D201-86 #in-progress 펀딩만들기 메인화면 페이지 완료

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,9 +1,10 @@
 import MyFunding from "./components/Funding/pages/MyFunding"
+import MakeFundingMain from "./components/Funding/pages/MakeFundingMain"
 
 function App() {
 
   return (
-    <MyFunding/>
+    <MakeFundingMain/>
   )
 }
 
