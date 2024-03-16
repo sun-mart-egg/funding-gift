1615c512f978b2476c289448ee39f5128b2fc3f5
parkjonghyuk <occultism22@naver.com>
2024-03-16 22:58:59 +0900

    [FE][Feat] #S10P22D201-94 #done 회원가입완료 초기설정

---
+++ FE/src/components/Login/SignupFinish.jsx
@@ -0,0 +1,12 @@
+function SignupFinish() {
+    return (
+        <div className=" w-[360px] h-[640px] bg-white flex flex-col items-center">
+            <p className=" text-3xl text-cusColor1 font-cusFont6">환영합니다.</p>
+            <div className=" w-[360px] h-[220px] bg-cyan-300" />
+            <p className=" text-[15px] font-cusFont3">회원가입이 완료되었습니당</p>
+            <button className=' w-[285px] h-[48px] font-cusFont2 text-white rounded-lg bg-cusColor3'>로그인 화면으로 이동</button>
+        </div>
+    )
+}
+
+export default SignupFinish
