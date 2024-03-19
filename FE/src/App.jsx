29c5185a14bbec4136d85952db327014d88fe33a
신시은 <nssu422@naver.com>
2024-03-20 00:00:28 +0900

    [FE][Feat] #S10P22D201-142 #done 펀딩만들기/정보입력 초기 화면 완성

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -7,16 +7,23 @@ import ProductDetail from "./components/Products/ProductDetail";
 import BrandStore from "./components/Products/BrandStore";
 import Wishlist from "./components/Products/Wishlist";
 import Signup from "./components/Login/Signup.jsx";
-
+import MakeFundingDetail from "./components/Funding/pages/MakeFundingDetail.jsx";
+import MakeFundingFinish from "./components/Funding/pages/MakeFundingFinish.jsx";
 function App() {
     return (
         <>
             <BrowserRouter>
                 <Routes>
+                    <Route path="/" element={<MakeFundingDetail />} /> {/* 루트 경로 설정 */}
+                    <Route path="/my-funding" element={<MyFunding />} />
+                    <Route path="/make-funding" element={<MakeFundingMain />} />
+                    <Route path="/make-funding-detail" element={<MakeFundingDetail />} />
+                    <Route path="/make-funding-finish" element={<MakeFundingFinish />} /> {/* 이 부분 추가 */}
                     <Route path="/product" element={<Product />} />
                     <Route path="/product/:productId" element={<ProductDetail />} />
                     <Route path="/brand/:brandId" element={<BrandStore />} />
                     <Route path="/wishlist/:userId" element={<Wishlist />} />
+                    <Route path="/signup" element={<Signup />} />
                 </Routes>
             </BrowserRouter>
         </>
