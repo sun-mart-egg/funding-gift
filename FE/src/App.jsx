427e84e393c7d879d00aa621c9c7494f06f1a898
Park Jong Hyuk <occultism22@naver.com>
2024-03-22 04:02:30 +0900

    [FE][Feat] SubLayout 설정

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,5 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import MainLayout from "./components/UI/MainLayout.jsx";
+import SubLayout from "./components/UI/SubLayout.jsx";
+
+import Login from "./components/Login/Login.jsx"
+import Signup from "./components/Login/Signup.jsx";
+
 import MyFunding from "./components/Funding/pages/MyFunding";
 import AccountListPage from "./components/Funding/pages/AccountListPage.jsx";
 import MakeFundingMain from "./components/Funding/pages/MakeFundingMain";
@@ -8,17 +14,14 @@ import ProductDetail from "./components/Products/ProductDetail";
 import BrandStore from "./components/Products/BrandStore";
 import Wishlist from "./components/Products/Wishlist";
 import AddressListPage from "./components/Funding/pages/AddressListPage.jsx";
-import Signup from "./components/Login/Signup.jsx";
 import AnniversaryListPage from "./components/Funding/pages/AnniversaryListPage.jsx";
 import FriendFundingDetail from "./components/Funding/pages/FriendFundingDetail.jsx";
 import ParticipatePage from "./components/Funding/pages/ParticipatePage.jsx";
 import Paypage from "./components/Funding/pages/Paypage.jsx";
 import MakeFundingDetail from "./components/Funding/pages/MakeFundingDetail.jsx";
 import MakeFundingFinish from "./components/Funding/pages/MakeFundingFinish.jsx";
-import Login from "./components/Login/Login.jsx"
 import Home from "./components/Home/Home.jsx"
 
-import MainLayout from "./components/UI/MainLayout.jsx";
 
 function App() {
 
@@ -29,9 +32,14 @@ function App() {
           <Route element={<MainLayout />}>
             <Route path="/home" element={<Home />} />
             <Route path="/" element={<MyFunding />} />
-            <Route path="/login" element={<Login />} />
             <Route path="/product" element={<Product />} />
           </Route>
+
+          <Route element={<SubLayout />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />}/>
+          </Route>
+
           <Route path="/make-funding-detail" element={<MakeFundingDetail />} />
           <Route path="/make-funding-main" element={<MakeFundingMain />} />
           <Route path="/participate" element={<ParticipatePage />} />
