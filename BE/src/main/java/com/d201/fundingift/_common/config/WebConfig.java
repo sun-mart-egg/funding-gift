3b201ec38f38dd0608895709353d3d4d4c71666d
박창준 <qmak01@naver.com>
2024-03-22 17:34:15 +0900

    [BE][Fix] import 제거

--- BE/src/main/java/com/d201/fundingift/_common/config/WebConfig.java
+++ BE/src/main/java/com/d201/fundingift/_common/config/WebConfig.java
@@ -1,11 +1,9 @@
 package com.d201.fundingift._common.config;
 
-import com.eni.backend.auth.jwt.JwtAuthInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
