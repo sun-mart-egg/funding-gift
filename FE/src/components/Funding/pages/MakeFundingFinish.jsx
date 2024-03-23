dd6e298bdb639b5e58f7d9049b3e087b5cbc218b
신시은 <nssu422@naver.com>
2024-03-23 22:49:50 +0900

    [FE][Fix] #S10P22D201-154 펀딩 완료 페이지 디자인 완

--- FE/src/components/Funding/pages/MakeFundingFinish.jsx
+++ FE/src/components/Funding/pages/MakeFundingFinish.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom"; // useNavigate 사용
+import { FaCheckCircle } from "react-icons/fa";
 
 function MakeFundingFinish() {
   const navigate = useNavigate(); // useNavigate 훅 사용
@@ -10,23 +11,48 @@ function MakeFundingFinish() {
     navigate("/my-funding");
   };
   return (
-    <div className="sub-layer">
+    <div
+      className="sub-layer font-cusFont3"
+      style={{
+        background: "linear-gradient(to bottom, #E5EEFF, #FFFFFF)", // 세로 그라디언트 정의
+      }}
+    >
       <div
         id="makeCard"
-        className="flex h-3/5 w-2/3 flex-col justify-around rounded-xl bg-white p-4"
+        className="mt-20 flex w-[75%] flex-col items-center justify-start rounded-xl bg-white p-4 shadow-md"
+        style={{ height: "68%" }}
       >
-        <div id="contentSection">펀딩 성공적으로 만들었음 ㅊㅋㅊㅋ</div>
-      </div>
-      <div id="buttonSection" className="flex w-full justify-around">
-        <button
-          onClick={() => navigate("/")}
-          className="rounded-lg bg-cusColor3"
+        <div
+          id="contentSection"
+          className="m-auto flex flex-col items-center justify-center"
         >
-          홈화면으로
-        </button>
-        <button onClick={navigateMypage} className="rounded-lg bg-cusColor4">
-          내 펀딩화면으로{" "}
-        </button>
+          <p className="mb-2 font-cusFont2 text-[18px]">
+            펀딩을 성공적으로 등록했습니다!
+          </p>
+          <p className="font-cusFont3">지금 바로 확인해 보세요!</p>
+          <FaCheckCircle className="mt-2 size-6 text-cusColor1" />
+        </div>
+      </div>
+      <div
+        id="buttonSection"
+        className="absolute bottom-0 w-full justify-around pb-5"
+      >
+        <div className="flex justify-center space-x-4">
+          <button
+            onClick={() => navigate("/")}
+            style={{ width: "calc(40% - 10px)" }} // 버튼 너비 조정
+            className="common-btn border-cus border border-cusColor3 bg-white text-black "
+          >
+            홈화면으로
+          </button>
+          <button
+            onClick={() => navigate("/my-funding")}
+            style={{ width: "calc(40% - 10px)" }} // 버튼 너비 조정
+            className="common-btn"
+          >
+            내 펀딩화면으로{" "}
+          </button>
+        </div>
       </div>
     </div>
   );
