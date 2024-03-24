7f165d0e1228f3925b05920f9107505fcd0849fa
limsubinn <mymyx02@naver.com>
2024-03-24 19:39:00 +0900

    [BE][Feat] PathVariable, RequestParam 예외 처리

--- BE/src/main/java/com/d201/fundingift/_common/exception/GlobalExceptionHandler.java
+++ BE/src/main/java/com/d201/fundingift/_common/exception/GlobalExceptionHandler.java
@@ -7,13 +7,14 @@ import org.hibernate.TypeMismatchException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import static com.d201.fundingift._common.response.ErrorType.METHOD_NOT_SUPPORTED;
-import static com.d201.fundingift._common.response.ErrorType.URL_NOT_FOUND;
+import static com.d201.fundingift._common.response.ErrorType.*;
 
 @Slf4j
 @EnableWebMvc
@@ -44,6 +45,22 @@ public class GlobalExceptionHandler {
         log.error("urlException throw Exception : {}", (Object) e.getStackTrace());
 
         return ResponseEntity.badRequest().body(ResponseUtils.error(URL_NOT_FOUND));
+    }
+
+    // path variable error
+    @ExceptionHandler(MissingPathVariableException.class)
+    public ResponseEntity<ErrorResponse> pathVariableException(Exception e) {
+        log.error("pathVariableException throw Exception : {}", (Object) e.getStackTrace());
+
+        return ResponseEntity.badRequest().body(ResponseUtils.error(PATH_VARIABLE_NOT_FOUND));
+    }
+
+    // request param error
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ErrorResponse> requestParamException(Exception e) {
+        log.error("requestParamException throw Exception : {}", (Object) e.getStackTrace());
+
+        return ResponseEntity.badRequest().body(ResponseUtils.error(REQUEST_PARAM_NOT_FOUND));
     }
 
     // custom error
--- BE/src/main/java/com/d201/fundingift/_common/response/ErrorType.java
+++ BE/src/main/java/com/d201/fundingift/_common/response/ErrorType.java
@@ -8,6 +8,8 @@ public enum ErrorType {
 
     METHOD_NOT_SUPPORTED(HttpStatus.BAD_REQUEST, "지원되지 않는 Http Method 입니다."),
     URL_NOT_FOUND(HttpStatus.BAD_REQUEST, "잘못된 URL 입니다."),
+    PATH_VARIABLE_NOT_FOUND(HttpStatus.BAD_REQUEST, "Path Variable 이 없습니다."),
+    REQUEST_PARAM_NOT_FOUND(HttpStatus.BAD_REQUEST, "Request Param 이 없습니다."),
 
     USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "등록된 사용자가 없습니다."),
     SORT_NOT_FOUND(HttpStatus.BAD_REQUEST, "정렬 조건을 찾을 수 없습니다."),
