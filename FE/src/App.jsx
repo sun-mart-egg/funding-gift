7ad0581cdc8d45f4be6b2424aa0812fb36555e50
Park Jong Hyuk <occultism22@naver.com>
2024-03-25 00:26:34 +0900

    [FE][Feat] #S10P22D201-134 친구목록 더미데이터로 테스트

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -7,6 +7,8 @@ import Login from "./components/Login/Login.jsx";
 import Signup from "./components/Login/Signup.jsx";
 import SignupFinish from "./components/Login/SignupFinish.jsx"
 
+import Friends from "./components/Friends/Friends.jsx"
+
 import MyFunding from "./components/Funding/pages/MyFunding";
 import AccountListPage from "./components/Funding/pages/AccountListPage.jsx";
 import MakeFundingMain from "./components/Funding/pages/MakeFundingMain";
@@ -44,6 +46,7 @@ function App() {
           <Route path="/make-funding-finish" element={<MakeFundingFinish />} />
           <Route path="/product/:productId" element={<ProductDetail />} />
           <Route path="/anniversary-list" element={<AnniversaryListPage />} />
+          <Route path="/friends" element={<Friends />} />
         </Route>
 
         <Route path="/participate" element={<ParticipatePage />} />
