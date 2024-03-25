95f1c326ad38935ebe0dc12d44720ebf0df838b7
limsubinn <mymyx02@naver.com>
2024-03-26 04:14:02 +0900

    [BE][Fix] cors 에러 url 수정

--- BE/src/main/java/com/d201/fundingift/_common/config/WebConfig.java
+++ BE/src/main/java/com/d201/fundingift/_common/config/WebConfig.java
@@ -28,7 +28,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", baseUrl)
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true)
                 .maxAge(3000);
