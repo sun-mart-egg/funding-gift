bc87cf5ef8253789b6ffd768b93274ae08dadaec
신시은 <nssu422@naver.com>
2024-03-25 10:41:46 +0900

    [FE][Design] #S10P22D201-150 #done 펀딩 디테일 메세지 디자인 작업 완료

--- FE/src/components/Funding/component/CongratulateList.jsx
+++ FE/src/components/Funding/component/CongratulateList.jsx
@@ -2,7 +2,7 @@ import CongratulateCard from "./CongratulateCard";
 
 function CongratulateList({ listData, onCardClick }) {
   return (
-    <div id="messageSection" className="w-full flex-col px-7 ">
+    <div id="messageSection" className=" w-full flex-col px-7 ">
       <div id="subTitle" className="px-2 font-cusFont2 text-[18px]">
         <p>축하 받은 리스트</p>
       </div>
