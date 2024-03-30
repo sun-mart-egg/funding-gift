957b78f287215252d8e2f792e7a4d367e39606e3
신시은 <nssu422@naver.com>
2024-03-31 00:33:03 +0900

    [FE][Design] 기념일 추가 페이지  디자인 살짝 수정

--- FE/src/components/Funding/pages/NewAnniversaryPage.jsx
+++ FE/src/components/Funding/pages/NewAnniversaryPage.jsx
@@ -6,20 +6,20 @@ function NewAnniversaryPage() {
       <div className="title flex w-full items-center justify-center ">
         <p className="absolute top-20 font-cusFont5 text-[30px]">기념일 추가</p>
       </div>
-      <div className="content absolute top-32 w-full p-4">
+      <div className="content absolute top-32 w-full p-4 font-cusFont3">
         <div className="anniversaryName mb-6">
-          <p className="mb-2 font-cusFont2 text-[20px]">기념일 이름</p>
+          <p className="mb-2 font-cusFont2 text-[18px]">기념일 이름</p>
           <input
             type="text"
             placeholder="기념일의 이름을 입력하세요 ex)생일"
-            className="h-[44px] w-full rounded-md border border-[#9B9B9B] px-2 placeholder:text-[14px]"
+            className="h-[44px] w-full rounded-md border border-[#9B9B9B] px-2 placeholder:text-[14px] focus:border-cusColor3 focus:outline-none"
           />
         </div>
         <div className="anniversaryDate">
-          <p className="mb-2 font-cusFont2 text-[20px]">기념일 날짜</p>
+          <p className="mb-2 font-cusFont2 text-[18px]">기념일 날짜</p>
           <input
             type="date"
-            className="h-[44px] w-full rounded-md border border-[#9B9B9B] px-2"
+            className="h-[44px] w-full rounded-md border border-[#9B9B9B] px-2 focus:border-cusColor3 focus:outline-none"
           />
         </div>
       </div>
