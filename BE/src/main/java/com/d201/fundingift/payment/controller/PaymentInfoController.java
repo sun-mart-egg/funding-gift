024c0ff02ffe1332a3bf3c79c52eb4e7e5253f19
kdozlo <eodud5730@naver.com>
2024-04-03 17:05:33 +0900

    [BE][Feat] #S10P22D201-184 결제 정보 검증 및 저장 api 스웨거 설명 추가

--- BE/src/main/java/com/d201/fundingift/payment/controller/PaymentInfoController.java
+++ BE/src/main/java/com/d201/fundingift/payment/controller/PaymentInfoController.java
@@ -1,5 +1,6 @@
 package com.d201.fundingift.payment.controller;
 
+import com.d201.fundingift._common.response.ErrorResponse;
 import com.d201.fundingift._common.response.ResponseUtils;
 import com.d201.fundingift._common.response.SuccessResponse;
 import com.d201.fundingift._common.response.SuccessType;
@@ -7,6 +8,11 @@ import com.d201.fundingift.payment.dto.request.PostPaymentInfoRequest;
 import com.d201.fundingift.payment.service.PaymentInfoService;
 import com.siot.IamportRestClient.response.IamportResponse;
 import com.siot.IamportRestClient.response.Payment;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +29,21 @@ public class PaymentInfoController {
 
     private final PaymentInfoService paymentInfoService;
 
+    @Operation(summary = "결제 정보 검증 및 저장",
+            description = """
+                           `token` \n
+                           결제 정보를 검증하고 저장합니다. \n
+                           """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "성공",
+                    useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "400",
+                    description = "로그인 여부 / 결제 완료 여부 / 펀딩 참여 금액과 실제 결제 금액 비교",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    ))
+    })
     @PostMapping
     public SuccessResponse<IamportResponse<Payment>> postPaymentInfo(@RequestBody @Valid PostPaymentInfoRequest postPaymentInfoRequest) {
 
