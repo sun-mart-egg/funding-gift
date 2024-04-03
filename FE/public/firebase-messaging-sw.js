df3803ae07141e6380c5967a62dfce8d67fc7724
limsubinn <mymyx02@naver.com>
2024-04-04 00:11:58 +0900

    [FE][Fix] 서비스워커 파일 수정
    (백그라운드 알림용)

--- FE/public/firebase-messaging-sw.js
+++ FE/public/firebase-messaging-sw.js
@@ -1,33 +1,39 @@
-import { initializeApp } from "firebase/app";
-import { getMessaging, onBackgroundMessage } from "firebase/messaging/sw";
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
-const firebaseApp = initializeApp({
-    apiKey: 'AIzaSyBE1OaWA2Bo3bxh-8oUfJCKGGFz6DkNYbA',
-    authDomain: 'funding-gift.firebaseapp.com',
-    // databaseURL: 'https://project-id.firebaseio.com',
-    projectId: 'funding-gift',
-    storageBucket: 'funding-gift.appspot.com',
-    messagingSenderId: '184194517827',
-    appId: '1:184194517827:web:f2a715c4f6c082503afdf6',
-    measurementId: 'G-GPCQJX1FSL',
-  });
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: 'AIzaSyBE1OaWA2Bo3bxh-8oUfJCKGGFz6DkNYbA',
+  authDomain: 'funding-gift.firebaseapp.com',
+//   databaseURL: 'https://project-id.firebaseio.com',
+  projectId: 'funding-gift',
+  storageBucket: 'funding-gift.appspot.com',
+  messagingSenderId: '184194517827',
+  appId: '1:184194517827:web:f2a715c4f6c082503afdf6',
+  measurementId: 'G-GPCQJX1FSL',
+});
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
-const messaging = getMessaging(firebaseApp);
+const messaging = firebase.messaging();
 
-onBackgroundMessage(messaging, (payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
-
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
-});
+messaging.onBackgroundMessage((payload) => {
+    console.log(
+      "background message",
+      payload
+    );
+    
+    // Customize notification here
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+      body: payload.notification.body,
+    };
+    
+    self.registration.showNotification(notificationTitle, notificationOptions);
+  });
